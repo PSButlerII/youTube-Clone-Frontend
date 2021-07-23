cd4914c6a19c75faf71f4c2cd20674cef4c2cec9
Jordan-Edwards <jordanedwards441@gmail.com>
2021-07-23 13:37:02 -0500

    Moved 'SearchVideo' to it's own Component / Added render method to 'app.jsk' / [Need to Connect 'SearchVideo' to 'MainVideo']

--- src/app.jsx
+++ src/app.jsx
@@ -2,57 +2,23 @@ import React, { Component } from 'react';
 import SearchVideo from './components/searchVideo/searchVideo';
 import axios from 'axios';
 import MainVideo from './components/mainVideo/mainVideo';
-
-
+import searchVideo from './components/searchVideo/searchVideo';
 class App extends Component {
+   
     constructor(props) {
         super(props);
         this.state = {
-            videos: [
-                {videoId: ''}
-            ],
-            search_input: ''
+            videos: []
         }
     }
 
-
-   onChangeHandler = (event) => {
-       this.setState({
-           [event.target.name]: event.target.value
-       });
-       console.log(this.state.search_input);
-   }
-
-   onSubmitHandler = (event) => {
-       event.preventDefault();
-       this.youTubeVideo();
-   }
-
-
-    youTubeVideo = async (event) => {
-        let response = await axios.get(`https://www.googleapis.com/youtube/v3/search?q=${this.state.search_input}&part=snippet&key=AIzaSyB3F_ppVkCcneSQJ6JiuctLOK503A4cOCE`)
-        console.log(response)
-        this.setState({
-            videos: response.data
-        });
-        console.log(this.state.videos)
-    }
-
-    displayVideos = () => {
-        return this.videos;
-    }
-
-    render() { 
-        return ( 
-            <React.Fragment>
-                <form onSubmit={this.onSubmitHandler}>
-                <input type="text" name="search_input" onChange={this.onChangeHandler} value={this.state.search_input} placeholder="Search Video" />
-                    <button type="submit" className="btn btn-primary w-md-25">Search</button>
-                </form>
-                {this.displayVideos}
-            </React.Fragment>
-         );
-    }
+render() {
+    return(
+        <React.Fragment>
+            <SearchVideo videos={this.state.videos} />
+            <MainVideo/>
+        </React.Fragment>
+    )
+  }
 }
- 
 export default App;
--- src/components/searchVideo/searchVideo.jsx
+++ src/components/searchVideo/searchVideo.jsx
@@ -1,50 +1,54 @@
-import React, { Component } from "react";
+import axios from 'axios'
+import React, {Component} from 'react'
 
+class searchVideo extends Component {
 
-class SearchVideo extends Component {
   constructor(props) {
-    super(props);
-    this.state = {
-      searchTerm: "",
-    };
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
+  super(props);
+  this.state = {
+      videos: [
+          {videoId: ''}
+      ],
+      search_input: ''
+  }
   }
 
-  handleChange = (event) => {
-    this.setState({
-      searchTerm: event.target.value,
-    });
-    console.log(event.target.value);
-  };
+  onChangeHandler = (event) => {
+  this.setState({
+      [event.target.name]: event.target.value
+  });
+  console.log(this.state.search_input);
+  }
 
-  handleSubmit = (event) => {
-    event.preventDefault();
-    const videoSearch = {         
-        searchtTerm: event.target.value
-        }
-    }
- 
-  render() {
-    return (
-      <div className="search-bar-center">
-    
-        <form className="form-inline" onSubmit={this.handleSubmit}>
-        <h2>Search Music</h2>
-          <input
-            type="text"
-            placeholder="Type to search..."
-            value={this.state.searchTerm}
-            onChange={this.handleChange}
-          />
-          <button type="submit">Search Videos</button>
-        </form>
-        
-                </div>
-      
-      
+  onSubmitHandler = (event) => {
+  event.preventDefault();
+  this.youTubeVideo();
+  }
+
+  youTubeVideo = async (event) => {
+  let response = await axios.get(`https://www.googleapis.com/youtube/v3/search?q=${this.state.search_input}&part=snippet&key=AIzaSyDz6KTK8HSbJFMN-O9EEw5PHugnIS-oaQE`)
+  console.log(response)
+  this.setState({
+      videos: response.data
+  });
+  console.log(this.state.videos)
+  }
+
+  displayVideos = () => {
+  return this.videos;
+  }
+
+  render() { 
+  return ( 
+      <React.Fragment>
+          <form onSubmit={this.onSubmitHandler}>
+          <input type="text" name="search_input" onChange={this.onChangeHandler} value={this.state.search_input} placeholder="Search Video" />
+              <button type="submit">Search</button>
+          </form>
+          {this.displayVideos}
+      </React.Fragment>
     );
   }
-}
+  }
 
-export default SearchVideo;
+  export default searchVideo;
