606233506b7f0f551de904e149370811e852655e
Jordan-Edwards <jordanedwards441@gmail.com>
2021-07-23 11:04:14 -0500

    Query now pulls user input from the search nar

--- src/app.jsx
+++ src/app.jsx
@@ -4,34 +4,52 @@ import axios from 'axios';
 import MainVideo from './components/mainVideo/mainVideo';
 
 
-
 class App extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            searchTerm: ""
-          }
-    }
-    
-    async youTubeVideos(e) {
-        let searchVideos = await axios.get(`https://www.googleapis.com/youtube/v3/search?q={this.state.searchTerm}&part=snippet&key=AIzaSyB3F_ppVkCcneSQJ6JiuctLOK503A4cOCE`)
-        .then(response => this.setState({
-            videos: response.data            
-        })     
-        );  
-        console.log(this)
+            videos: [
+                {videoId: ''}
+            ],
+            search_input: ''
+        }
     }
 
-    componentDidMount(){
-        this.youTubeVideos()
-    } 
+
+   onChangeHandler = (event) => {
+       this.setState({
+           [event.target.name]: event.target.value
+       });
+       console.log(this.state.search_input);
+   }
+
+   onSubmitHandler = (event) => {
+       event.preventDefault();
+       this.youTubeVideo();
+   }
+
+
+    youTubeVideo = async (event) => {
+        let response = await axios.get(`https://www.googleapis.com/youtube/v3/search?q=${this.state.search_input}&part=snippet&key=AIzaSyB3F_ppVkCcneSQJ6JiuctLOK503A4cOCE`)
+        console.log(response)
+        this.setState({
+            videos: response.data
+        });
+        console.log(this.state.videos)
+    }
+
+    displayVideos = () => {
+        return this.videos;
+    }
 
     render() { 
         return ( 
             <React.Fragment>
-                <h1>Test</h1>
-                <SearchVideo videos={this.state.videos} />
-                {/* <MainVideo />    */}
+                <form onSubmit={this.onSubmitHandler}>
+                <input type="text" name="search_input" onChange={this.onChangeHandler} value={this.state.search_input} placeholder="Search Video" />
+                    <button type="submit" className="btn btn-primary w-md-25">Search</button>
+                </form>
+                {this.displayVideos}
             </React.Fragment>
          );
     }
